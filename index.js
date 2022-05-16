df7793bdf17d30c86cbb9e4a527372fff76d11cf
Douglas-Lorenzi-Saldanha <103324394+Douglas-Lorenzi-Saldanha@users.noreply.github.com>
2022-05-16 01:13:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-console.log("Preço da Alcool: ")
+do{
+  resp = 'y'
+  alc = prompt('Preço da Alcool: ')
+  gas = prompt('Preço da Gasolina: ')
 
+  result = alc/gas
 
-console.log("Preço da Gasolina: ")
+  if(result > 0.7){
+    console.log('Vale a pena comprar Gasolina')
+  }
+  else if(result < 0.7){
+    console.log('Vale a pena comprar Alcool')
+  }
+  else{
+    console.log('Pode comprar qualquer um dos dois')
+  }
+
+    resp = prompt('Gostaria de executar o programa novamente(y/n): ')
+    console.clear();
+}while(resp != 'n');
+
+console.log('Fim da execução do programa. Tenha um ótimo dia')
